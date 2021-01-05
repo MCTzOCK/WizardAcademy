e4599f11cc464dad06b763804d8681934dfe7409
MCTzOCK <dev@mctzock.de>
2021-01-05 12:16:47 +0100

    Created basic Plugin structure

---
+++ src/main/java/net/craftions/wizardacademy/WizardAcademy.java
@@ -0,0 +1,17 @@
+package net.craftions.wizardacademy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WizardAcademy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

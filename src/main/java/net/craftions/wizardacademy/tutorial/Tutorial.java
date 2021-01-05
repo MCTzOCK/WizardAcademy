129bf21051b6e5b28c175b7161d7a2581a1fe396
Sausefix <felix@craftions.net>
2021-01-05 16:17:20 +0100

    Added Timer

--- src/main/java/net/craftions/wizardacademy/tutorial/Tutorial.java
+++ src/main/java/net/craftions/wizardacademy/tutorial/Tutorial.java
@@ -1,7 +1,9 @@
 package net.craftions.wizardacademy.tutorial;
 
+import net.craftions.wizardacademy.WizardAcademy;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class Tutorial {
@@ -9,15 +11,28 @@ public class Tutorial {
     public void TutorialText(Player p){
 
         p.sendMessage("§7§l Hallo " + p.getName() + ".");
-        p.sendMessage("§7§l Ich habe gehört, dass du neu auf der WizardAcademy bist.");
-        p.sendMessage("§7§l Was hältst du von einer Rundtour am Anfang.");
+        p.sendMessage("Hallo! Hier neben dir...");
 
-        TextComponent jaGerne = new TextComponent("§a Ja, gerne");
-        ClickEvent clickEventJa = new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/rundtour");
-        jaGerne.setClickEvent(clickEventJa);
-        p.spigot().sendMessage(jaGerne);
 
-        TextComponent neinDanke = new TextComponent("§c Nein, Danke");
+        Bukkit.getScheduler().scheduleSyncDelayedTask(WizardAcademy.plugin, new Runnable() {
+            @Override
+            public void run() {
 
+                p.sendMessage("§7§l Ich habe gehört, dass du neu auf der WizardAcademy bist.");
+                p.sendMessage("§7§l Was hältst du von einer Rundtour am Anfang.");
+                p.sendMessage("Ich bin übrigens (Otto)");
+
+                TextComponent jaGerne = new TextComponent("§a Ja, gerne");
+                ClickEvent clickEventJa = new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/rundtour ja");
+                jaGerne.setClickEvent(clickEventJa);
+                p.spigot().sendMessage(jaGerne);
+
+                TextComponent neinDanke = new TextComponent("§c Nein, Danke");
+                ClickEvent clickEventNein = new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/rundtour nein");
+                neinDanke.setClickEvent(clickEventNein);
+                p.spigot().sendMessage(neinDanke);
+
+            }
+        },2 * 20);
     }
 }

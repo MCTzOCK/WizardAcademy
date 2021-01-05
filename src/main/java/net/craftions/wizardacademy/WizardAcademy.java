d9f559d2c18461dc94b9bc92a49e5a2ee1073b52
MCTzOCK <dev@mctzock.de>
2021-01-05 12:26:28 +0100

    Changed Plugin.yml and added welcome message

--- src/main/java/net/craftions/wizardacademy/WizardAcademy.java
+++ src/main/java/net/craftions/wizardacademy/WizardAcademy.java
@@ -7,7 +7,7 @@ public final class WizardAcademy extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Welcome to WizardAcademy v" + this.getDescription().getVersion() + " by " + this.getDescription().getAuthors());
     }
 
     @Override
